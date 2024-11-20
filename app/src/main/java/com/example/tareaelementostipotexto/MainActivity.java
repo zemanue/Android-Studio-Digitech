53d3f0265753d597cf401351add811c8ab8d0654
zemanue <manureco.97@gmail.com>
2024-11-20 17:09:43 +0100

    Update MainActivity.java
    Asociate the edit text and the button with the main activity, and create the onClickListener (no text for the moment)

--- app/src/main/java/com/example/tareaelementostipotexto/MainActivity.java
+++ app/src/main/java/com/example/tareaelementostipotexto/MainActivity.java
@@ -4,7 +4,12 @@
 
 package com.example.tareaelementostipotexto;
 
+// Importamos las clases necesarias
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +18,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declaramos los objetos que hacen referencia a los elementos de la interfaz que hemos añadido.
+    private EditText userInput;
+    private Button sendInputButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +33,18 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Se enlazan las variables con los elementos de la interfaz
+        sendInputButton = findViewById(R.id.sendInputButton);
+        userInput = findViewById(R.id.userInput);
+
+        // Asociar un objeto OnClickListener al botón, que es el objeto que reacciona al click
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override // Hace falta sobrescribir el metodo onClick de esta interfaz indicándole qué debe hacer
+            public void onClick(View v) {
+                // Añadimos el mensaje de tipo Toast
+                Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
+            }
+        };
     }
 }
