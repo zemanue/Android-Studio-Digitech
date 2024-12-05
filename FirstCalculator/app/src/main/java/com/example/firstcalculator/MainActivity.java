aad3eebcaf9222061f585ea568d2ad52af52426a
zemanue <manureco.97@gmail.com>
2024-12-05 23:07:16 +0100

    Update MainActivity.java
    Elements of the interface declared and linked

--- FirstCalculator/app/src/main/java/com/example/firstcalculator/MainActivity.java
+++ FirstCalculator/app/src/main/java/com/example/firstcalculator/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.firstcalculator;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewResult;
+    private EditText editTextNum1, editTextNum2;
+    private RadioButton radioButtonSum, radioButtonSubst, radioButtonMult, radioButtonDiv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editTextNum1 = findViewById(R.id.editTextNum1);
+        editTextNum2 = findViewById(R.id.editTextNum2);
+        radioButtonSum = findViewById(R.id.radioButtonSum);
+        radioButtonSubst = findViewById(R.id.radioButtonSubst);
+        radioButtonMult = findViewById(R.id.radioButtonMult);
+        radioButtonDiv = findViewById(R.id.radioButtonDiv);
+        textViewResult = findViewById(R.id.textViewResult);
     }
 }
