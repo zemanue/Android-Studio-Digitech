f20e55a05e78a0367bbc9632b9630130ef5d5403
zemanue <manureco.97@gmail.com>
2024-12-12 13:23:42 +0100

    Update MainActivity.java
    Exercise finished: a Toast message appears informing which switches are activated

--- TareaSwitch/app/src/main/java/com/example/tareaswitch/MainActivity.java
+++ TareaSwitch/app/src/main/java/com/example/tareaswitch/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.tareaswitch;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Switch switchMobileData, switchWiFi;
+    Button buttonCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        switchMobileData = findViewById(R.id.switchMobileData);
+        switchWiFi = findViewById(R.id.switchWiFi);
+        buttonCheck = findViewById(R.id.buttonCheck);
+    }
+
+    public void checkConnection(View v){
+        String checkConnectionText;
+        if (switchMobileData.isChecked() && switchWiFi.isChecked()) {
+            checkConnectionText = "Los datos móviles y la conexión WiFi están activados.";
+        } else if (switchMobileData.isChecked()) {
+            checkConnectionText = "Solo los datos móviles están activados";
+        } else if (switchWiFi.isChecked()) {
+            checkConnectionText = "Solo la conexión WiFi está activada";
+        } else {
+            checkConnectionText = "Los datos móviles y la conexión WiFi están desactivados";
+        }
+
+        Toast.makeText(this, checkConnectionText, Toast.LENGTH_SHORT).show();
     }
 }
