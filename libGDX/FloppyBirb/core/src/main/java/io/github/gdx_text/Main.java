8b60b7d9d767f92edd2c926a6c941b7f970b0b49
zemanue <manureco.97@gmail.com>
2025-04-21 13:27:24 +0200

    Add bg and bird textures

--- libGDX/FloppyBirb/core/src/main/java/io/github/gdx_text/Main.java
+++ libGDX/FloppyBirb/core/src/main/java/io/github/gdx_text/Main.java
@@ -1,34 +1,38 @@
 package io.github.gdx_text;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
+
     private SpriteBatch batch;
-    private Texture image;
+    private Texture bg;
+    private Texture floppyBirb;
+
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        bg = new Texture("pixelartbg.png");
+        floppyBirb = new Texture("floppybirb1.png");
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f); // Clear screen with a blue color
         batch.begin();
-        batch.draw(image, 140, 210);
+        batch.draw(bg, 0, 0);
+        batch.draw(floppyBirb, 150, 480, 100, 100);
         batch.end();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        batch.dispose(); // Este metodo se llama cuando la aplicación se cierra para liberar recursos
+        floppyBirb.dispose();
+        bg.dispose();
     }
 }
